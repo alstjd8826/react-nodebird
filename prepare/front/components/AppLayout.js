ef13096773bae5550f869c672b9a2adf4e534252
minsung <alstjd8826>
2021-08-18 00:38:01 +0900

    2021/08/17

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+// next에서 라우터의 역할을 한다.
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
 	return (
 		<div>
-			<div>공통메뉴</div>
+			<div>
+				<Link href="/">노드버드</Link>
+				<Link href="/profile">프로필</Link>
+				<Link href="/signup">회원가입</Link>
+			</div>
 			{children}
 		</div>
 	);
